1cbf22039d24b228f21423bd5a18d7cd91d4bdb4
gex7777 <gex7777@gmail.com>
2019-11-24 19:38:43 +0530

    add firebase cdn

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyATJEMqHTqN0FlFAEUub7Tn4k5RU-IVOAc",
+    authDomain: "shop-54cb9.firebaseapp.com",
+    databaseURL: "https://shop-54cb9.firebaseio.com",
+    projectId: "shop-54cb9",
+    storageBucket: "shop-54cb9.appspot.com",
+    messagingSenderId: "266945871742",
+    appId: "1:266945871742:web:5b54edad512ed0c59cbd2f",
+    measurementId: "G-3S2QC89F3L"
+  }
 };
 
 /*
