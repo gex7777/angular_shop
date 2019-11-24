ca6c0bb021028c3e831cbbe8beff00227cef24b8
gex7777 <gex7777@gmail.com>
2019-11-24 23:30:31 +0530

    Revert "add navbar and routing"
    This reverts commit 55cc397c5dddea4ae60ba33c210305b3e0d93490.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,3 @@
-import { LoginComponent } from "./login/login.component";
-import { Router, RouterModule } from "@angular/router";
 import { environment } from "./../environments/environment";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
@@ -8,59 +6,15 @@ import { AngularFireDatabaseModule } from "@angular/fire/database";
 import { AngularFireAuthModule } from "@angular/fire/auth";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import {
-  MdcTopAppBarModule,
-  MdcIconModule,
-  MdcListModule,
-  MdcMenuModule,
-  MdcDrawerModule
-} from "@angular-mdc/web";
 
-import { MdcNavbarComponent } from "./mdc-navbar/mdc-navbar.component";
-import { HomeComponent } from "./home/home.component";
-import { ProductsComponent } from "./products/products.component";
-import { ShoppingCartComponent } from "./shopping-cart/shopping-cart.component";
-import { CheckOutComponent } from "./check-out/check-out.component";
-import { OrderSuccessComponent } from "./order-success/order-success.component";
-import { MyOrdersComponent } from "./my-orders/my-orders.component";
-import { AdminProductsComponent } from "./admin/admin-products/admin-products.component";
-import { AdminOrdersComponent } from "./admin/admin-orders/admin-orders.component";
 @NgModule({
-  declarations: [
-    AppComponent,
-    MdcNavbarComponent,
-    HomeComponent,
-    ProductsComponent,
-    ShoppingCartComponent,
-    CheckOutComponent,
-    OrderSuccessComponent,
-    MyOrdersComponent,
-    AdminProductsComponent,
-    AdminOrdersComponent,
-    LoginComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule,
-    MdcTopAppBarModule,
-    MdcIconModule,
-    MdcListModule,
-    MdcMenuModule,
-    MdcDrawerModule,
-    RouterModule.forRoot([
-      { path: "", component: HomeComponent },
-      { path: "products", component: ProductsComponent },
-      { path: "shopping-cart", component: ShoppingCartComponent },
-      { path: "check-out", component: CheckOutComponent },
-      { path: "order-success", component: OrderSuccessComponent },
-      { path: "login", component: LoginComponent },
-      { path: "admin/products", component: AdminProductsComponent },
-      { path: "admin/orders", component: AdminOrdersComponent },
-      { path: "my-orders", component: MyOrdersComponent }
-    ])
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
